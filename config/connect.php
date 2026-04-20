95363ed3938631272d5980f25528050ab987185b
testusermail44-cmyk <testusermail44@gmail.com>
2026-04-20 20:38:36 +0300

    Update connect.php

--- config/connect.php
+++ config/connect.php
@@ -1,5 +1,6 @@
 <?php
-    $conn = new mysqli("localhost", "root", "", "quizzy");
+    $pass = getenv('DB_PASS');
+    $conn = new mysqli("mysql-33ac47be-testusermail44-ffa1.a.aivencloud.com:25072", "avnadmin", $pass, "quizzy");
     if (!$conn || $conn->connect_errno) {
         die("DB error: " . $conn->connect_error);
     }
